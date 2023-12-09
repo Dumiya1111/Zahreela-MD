cbd6dc41ac043970fb8a86b11b0a1e656e8d7a45
ABDULLAH JUTT <113938161+abdullahjuttofc@users.noreply.github.com>
2023-12-08 23:45:41 -0800

    Update config.js

--- config.js
+++ config.js
@@ -3,24 +3,24 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER || "923192173398"  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
+global.owner = process.env.OWNER_NUMBER || "923277337485"  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://salmanahmad:s4salmanyt@cluster0.szcj2eo.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
-global.email = 'haseebyt@gmail.com'
+global.email = 'jutt420@gmail.com'
 global.github = 'https://github.com/abdullahjuttofc/Zahreela-MD'
 global.location = 'Lahore Pak'
-global.gurl = 'https://instagram.com/itz_shaikho/' // add your username
-global.sudo = process.env.SUDO || '923192173398' 
-global.devs = '923184070915';
-global.website = 'https://github.com/abdullahjuttofc/Zahreela-MD' //wa.me/+91000000000000
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/0ccbfa44449e7324b7790.jpg'
+global.gurl = 'https://instagram.com/' // add your username
+global.sudo = process.env.SUDO || '923277337485' 
+global.devs = '923277337485';
+global.website = 'https://github.com/abdullahjuttofc/Zahreela-MD' //wa.me/+923277337485
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/b4ef1cb678632811bf156.jpg'
 module.exports = {
   sessionName:  process.env.SESSION_ID ||  " ",  //PUT Session Id Here, 
-  author:  process.env.PACK_AUTHER ||  'wasi',
-  packname:  process.env.PACK_NAME || 'tech',
+  author:  process.env.PACK_AUTHER ||  'ZAHREELA-MD',
+  packname:  process.env.PACK_NAME || 'ZAHREELA-MD',
    
-  botname:   process.env.BOT_NAME === undefined ? "zarheela-Md" : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'haseeb' : process.env.OWNER_NAME,  
+  botname:   process.env.BOT_NAME === undefined ? "ZAHREELA-MD" : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'Abdullah' : process.env.OWNER_NAME,  
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
