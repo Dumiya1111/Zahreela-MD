1a8c3fc7eeed73d7e1a068d4638327c1a123ffa6
ABDULLAH JUTT <113938161+abdullahjuttofc@users.noreply.github.com>
2023-12-10 11:08:12 +0500

    @abdullahjuttofc

--- src/_updater.js
+++ src/_updater.js
@@ -3,10 +3,10 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : XLICON-MD
- * @author : salmanytofficial <https://github.com/salmanytofficial/XLICON-MD>
- * @description : XLICON,A Multi-functional whatsapp bot.
- * @version 0.0.6
+ * @project_name : ZAHREELA-MD
+ * @author : abdullahjuttofc <https://github.com/abdullahjuttofc/Zahreela-MD>
+ * @description : ZAHREELA,A Multi-functional whatsapp bot.
+ * @version 0.0.1
  **/
 
  
