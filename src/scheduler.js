3179f24034f44c2f4db09817bfcb7491621269c8
ABDULLAH JUTT <113938161+abdullahjuttofc@users.noreply.github.com>
2023-12-10 11:43:57 +0500

    @abdullahjuttofc

--- src/scheduler.js
+++ src/scheduler.js
@@ -3,10 +3,10 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
- * @version 0.0.6
+ * @project_name : ZAHREELA-MD
+ * @author : abdullahjuttofc <https://github.com/abdullahjuttofc>
+ * @description : ZAHREELA,A Multi-functional whatsapp bot.
+ * @version 0.0.1
  **/
 
  const { tlang,sck,prefix,cmd } = require('../lib')
